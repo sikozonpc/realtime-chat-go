16fafe34b06607429bb978d2de26a477464d67d5
Tiago Taquelim <tiago.taquelim.ferreira@gmail.com>
2024-02-05 21:04:39 +0000

    chore: add concurrent safe mutex in hub

--- hub.go
+++ hub.go
@@ -1,15 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
+	sync.RWMutex
+	
 	clients map[*Client]bool
 
 	broadcast  chan *Message
 	register   chan *Client
 	unregister chan *Client
+
+	messages []*Message
 }
 
 func NewHub() *Hub {
@@ -32,15 +39,28 @@ func (h *Hub) run() {
 	for {
 		select {
 		case client := <-h.register:
+			h.Lock()
 			h.clients[client] = true
+			h.Unlock()
+
 			log.Printf("client registered %s", client.id)
+
+
+			for _, msg := range h.messages {
+				client.send <- msg
+			}
 		case client := <-h.unregister:
+			h.Lock()
 			if _, ok := h.clients[client]; ok {
 				close(client.send)
 				log.Printf("client unregistered %s", client.id)
 				delete(h.clients, client)
 			}
+			h.Unlock()
 		case msg := <-h.broadcast:
+			h.RLock()
+			h.messages = append(h.messages, msg)
+
 			for client := range h.clients {
 				select {
 				case client.send <- msg:
@@ -49,6 +69,7 @@ func (h *Hub) run() {
 					delete(h.clients, client)
 				}
 			}
+			h.RUnlock()
 		}
 	}
 }
